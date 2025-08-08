ffacc6ce44ec8089671a662ec1d5716ce8bf40d2
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2025-08-08 10:02:42 -0400

    python -u rename_bam.py output unbuffered

--- launch_pipeline.py
+++ launch_pipeline.py
@@ -372,7 +372,7 @@ def dorado(toml_config):
 
     # Samtools
     dependencies = ":".join([f"$demux_{code}" for code in codes])
-    command3 = ["python", "/lustre09/project/6019267/shared/tools/main_pipelines/long-read/LongReadSequencingONT/rename_bam.py", toml_config["general"]["project_path"] + '/scripts/config_final.toml']
+    command3 = ["python", "-u", "/lustre09/project/6019267/shared/tools/main_pipelines/long-read/LongReadSequencingONT/rename_bam.py", toml_config["general"]["project_path"] + '/scripts/config_final.toml']
     command_str3 = " ".join(command3)
     time3 = "00-23:00"
     job3 = create_script("samtools", "8", "32", time3, output, email, command_str3, "")
