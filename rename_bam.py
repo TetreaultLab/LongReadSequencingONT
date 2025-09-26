321c2ef1350e370203179b0ad5072653a2e872fb
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2025-09-26 14:29:24 -0400

    str(to_dos) and add try expcept to rename_bam.py

--- rename_bam.py
+++ rename_bam.py
@@ -5,93 +5,98 @@ import argparse
 import toml
 import subprocess
 import ast
+import sys
 
-parser = argparse.ArgumentParser(
-        prog="Rename Bam files after dorado demultiplexing",
-        description="Change long names for bam files to a easier format",
-    )
+try:
+    parser = argparse.ArgumentParser(
+            prog="Rename Bam files after dorado demultiplexing",
+            description="Change long names for bam files to a easier format",
+        )
 
-parser.add_argument(
-    "--config", type=str, required=True, help="Project config file, including path."
-    )
-parser.add_argument(
-    "--flowcells", type=str, required=True, help="Flowcells to run."
-    )
+    parser.add_argument(
+        "--config", type=str, required=True, help="Project config file, including path."
+        )
+    parser.add_argument(
+        "--flowcells", type=str, required=True, help="Flowcells to run."
+        )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# Loading TOML config
-with open(args.config, "r") as f:
-    toml_config = toml.load(f)
+    # Loading TOML config
+    with open(args.config, "r") as f:
+        toml_config = toml.load(f)
 
-flowcells = ast.literal_eval(args.flowcells)
+    flowcells = ast.literal_eval(args.flowcells)
 
-all_inputs = []
-# Loop over flowcells to rename
-for fc in fcs :
-    print("\nRunning: rename for flowcell ", fc)
-    code = fc.split('_')[-1]
+    all_inputs = []
+    # Loop over flowcells to rename
+    for fc in fcs :
+        print("\nRunning: rename for flowcell ", fc)
+        code = fc.split('_')[-1]
 
-    inputs = toml_config["general"]["project_path"] + "/" + fc + "/alignments"
-    inputs = Path(inputs)
-    all_inputs.append(inputs)
+        inputs = toml_config["general"]["project_path"] + "/" + fc + "/alignments"
+        inputs = Path(inputs)
+        all_inputs.append(inputs)
 
-    output = toml_config["general"]["project_path"] + "/alignments"
-    output_path = Path(output)
+        output = toml_config["general"]["project_path"] + "/alignments"
+        output_path = Path(output)
 
-    # Load the CSV file
-    df = pd.read_csv(toml_config["general"]["project_path"] + "/scripts/" + fc + ".csv", header=0)
-    df["code"] = df["flow_cell_id"].str.split('_').str[-1]
+        # Load the CSV file
+        df = pd.read_csv(toml_config["general"]["project_path"] + "/scripts/" + fc + ".csv", header=0)
+        df["code"] = df["flow_cell_id"].str.split('_').str[-1]
 
-    # Create a mapping from barcode -> alias
-    barcode_to_alias = dict(zip(zip(df["barcode"], df["alias"]), df["code"]))
+        # Create a mapping from barcode -> alias
+        barcode_to_alias = dict(zip(zip(df["barcode"], df["alias"]), df["code"]))
 
-    # Process each file in the directory
-    for file in inputs.iterdir():
-        if file.is_file() and file.name.startswith(code):
-            for (barcode, alias), code in barcode_to_alias.items():
-                if barcode in file.name:
-                    if file.name.endswith(".bam"):
-                        new_name = f"{alias}_{barcode}_{code}.bam"
-                    else:
-                        continue
-                        
-                    new_path = inputs / new_name
-                    file.rename(new_path)
-                    print(f"Renamed {file.name} -> {new_name}")
+        # Process each file in the directory
+        for file in inputs.iterdir():
+            if file.is_file() and file.name.startswith(code):
+                for (barcode, alias), code in barcode_to_alias.items():
+                    if barcode in file.name:
+                        if file.name.endswith(".bam"):
+                            new_name = f"{alias}_{barcode}_{code}.bam"
+                        else:
+                            continue
+                            
+                        new_path = inputs / new_name
+                        file.rename(new_path)
+                        print(f"Renamed {file.name} -> {new_name}")
 
 
-# Merge bams
-samples = toml_config["general"]["samples"]
+    # Merge bams
+    samples = toml_config["general"]["samples"]
 
-# Loop over samples
-for s in samples:
-    print("\nRunning: Samtools for sample ", s)
-    output_file = output_path / f"{s}.bam"
+    # Loop over samples
+    for s in samples:
+        print("\nRunning: Samtools for sample ", s)
+        output_file = output_path / f"{s}.bam"
 
-    bam_files = []
-    for p in all_inputs:
-        bam_files.extend(p.glob(f"{s}_*.bam"))
+        bam_files = []
+        for p in all_inputs:
+            bam_files.extend(p.glob(f"{s}_*.bam"))
 
-    bam_files_str = [str(p) for p in bam_files]
+        bam_files_str = [str(p) for p in bam_files]
 
-    # merge
-    print("--> Merge")
-    cmd = ["samtools", "merge", "-f", "--threads", "3", "-o", str(output_file)] + bam_files_str
-    print(" ".join(cmd))
-    subprocess.run(cmd, check=True)
+        # merge
+        print("--> Merge")
+        cmd = ["samtools", "merge", "-f", "--threads", "3", "-o", str(output_file)] + bam_files_str
+        print(" ".join(cmd))
+        subprocess.run(cmd, check=True)
 
-    # sort
-    print("--> Sort + Index")
-    cmd2 = ["samtools", "sort", "--threads", "3", "-m", "4G", "-o", output + "/" + s + "_sorted.bam", output + "/" + s + ".bam"]
-    print(" ".join(cmd2))
-    subprocess.run(cmd2, check=True)
-    
-    # index
-    print("--> Index")
-    cmd3 = ["samtools", "index", "--threads", "3", "-o", output + "/" + s + "_sorted.bam.bai", output + "/" + s + "_sorted.bam"]
-    print(" ".join(cmd3))
-    subprocess.run(cmd3, check=True)
+        # sort
+        print("--> Sort + Index")
+        cmd2 = ["samtools", "sort", "--threads", "3", "-m", "4G", "-o", output + "/" + s + "_sorted.bam", output + "/" + s + ".bam"]
+        print(" ".join(cmd2))
+        subprocess.run(cmd2, check=True)
+        
+        # index
+        print("--> Index")
+        cmd3 = ["samtools", "index", "--threads", "3", "-o", output + "/" + s + "_sorted.bam.bai", output + "/" + s + "_sorted.bam"]
+        print(" ".join(cmd3))
+        subprocess.run(cmd3, check=True)
 
-print("Rename and Samtools done !")
+    print("Rename and Samtools done !")
+except Exception as e:
+    print(f"Error: {e}")
+    sys.exit(1)
 
