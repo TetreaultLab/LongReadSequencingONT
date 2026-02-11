1e388acd61b4dd9198424429c4920bf05e0f8bad
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2026-02-11 14:28:40 -0500

    remove function and add prints

--- samtools_multiprocess.py
+++ samtools_multiprocess.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import argparse
 import toml
 import subprocess
-import ast
 import sys
 
 try:
@@ -41,49 +40,51 @@ try:
 
     s = args.sample
 
-    def process_sample(s):
-        print(f"\nRunning Samtools for sample {s}")
-        output_file = output_path / f"{s}.bam"
+    print(f"\nRunning Samtools for sample {s}")
+    output_file = output_path / f"{s}.bam"
 
-        bam_files = []
-        for p in all_inputs:
-            bam_files.extend(p.glob(f"{s}_*.bam"))
+    bam_files = []
+    for p in all_inputs:
+        bam_files.extend(p.glob(f"{s}_*.bam"))
 
-        bam_files_str = [str(p) for p in bam_files]
+    bam_files_str = [str(p) for p in bam_files]
 
-        # merge
-        cmd = [
-            "samtools",
-            "merge",
-            "-c",
-            "-f",
-            "--threads",
-            "3",
-            "-o",
-            str(output_file),
-        ] + bam_files_str
-        subprocess.run(cmd, check=True)
+    # merge
+    print("Merge")
+    cmd = [
+        "samtools",
+        "merge",
+        "-c",
+        "-f",
+        "--threads",
+        "3",
+        "-o",
+        str(output_file),
+    ] + bam_files_str
+    subprocess.run(cmd, check=True)
 
-        # sort
-        sorted_bam = output_path / f"{s}_sorted.bam"
-        cmd2 = [
-            "samtools",
-            "sort",
-            "--threads",
-            "3",
-            "-m",
-            "4G",
-            "-o",
-            str(sorted_bam),
-            str(output_file),
-        ]
-        subprocess.run(cmd2, check=True)
+    # sort
+    print("Sort")
+    sorted_bam = output_path / f"{s}_sorted.bam"
+    cmd2 = [
+        "samtools",
+        "sort",
+        "--threads",
+        "3",
+        "-m",
+        "4G",
+        "-o",
+        str(sorted_bam),
+        str(output_file),
+    ]
+    subprocess.run(cmd2, check=True)
 
-        # index
-        cmd3 = ["samtools", "index", "--threads", "3", str(sorted_bam)]
-        subprocess.run(cmd3, check=True)
+    # index
+    print("Index")
+    cmd3 = ["samtools", "index", "--threads", "3", str(sorted_bam)]
+    subprocess.run(cmd3, check=True)
 
-        print(f"Done Samtools for {s}")
+    print(f"Done Samtools for {s}")
 
 except Exception as e:
     print(f"Error: {e}")
