f63aab17a76c9a11bdf71776174b191615d2e80a
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2026-03-30 17:06:54 -0400

    print

--- compare_samples/pipeline_LRS_compare.py
+++ compare_samples/pipeline_LRS_compare.py
@@ -117,9 +117,8 @@ def make_manifest_quantify(df, toml_config):
     username = os.environ.get("USER")
     scratch = f"/lustre10/scratch/{username}/{name}/flair_diff"
 
-    df["batch"] = get_project_name(df["project_path"])
-
-    df["batch"] = df["batch"].str.replace("_", "-")
+    df["batch"] = get_project_name(df["project_path"]).str.replace("_", "-")
+    print(df)
 
     df["fastq"] = scratch + "/fastq/" + df["samples"] + ".fastq"
 
