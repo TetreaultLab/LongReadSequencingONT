38e447f29ec5319c5fe8bed6e377624bfc6091cb
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2026-03-30 17:08:51 -0400

    print

--- compare_samples/pipeline_LRS_compare.py
+++ compare_samples/pipeline_LRS_compare.py
@@ -91,6 +91,7 @@ def read_metadata(toml_config):
 def get_project_name(path):
     # Search for the date pattern followed by the name
     match = re.search(r"\d{4}-\d{2}-\d{2}_([^/]+)", path)
+    print(match)
     return match.group(1) if match else None
 
 
