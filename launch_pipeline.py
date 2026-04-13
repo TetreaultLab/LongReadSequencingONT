273b4eb785352bdb674727954c4b6b6d9a847c0e
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2026-04-13 14:12:25 -0400

    dorado demux time

--- launch_pipeline.py
+++ launch_pipeline.py
@@ -807,7 +807,7 @@ def dorado_demux(toml_config, done):
         size_str = result.stdout.split()[0].rstrip("G")
 
         # Scale required job time based on amount of data
-        hours = int(size_str) * 0.001
+        hours = int(size_str) * 0.02
         formatted_time = format_time(hours)
 
         command = [
