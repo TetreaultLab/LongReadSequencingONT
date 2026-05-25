34c12063f33194e6025ed27a9d47b710c14d3c4f
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2026-05-25 09:42:37 -0400

    analyses

--- launch_LSK_jobs.py
+++ launch_LSK_jobs.py
@@ -31,7 +31,7 @@ for s in subfolders:
     toml_config_initial["general"]["email"] = email
 
     # Check if analyses are specified
-    if (toml_config_initial["general"]["analysis"] == "") and (analyses != ""):
+    if (toml_config_initial["general"]["analysis"] == [""]) and (analyses != ""):
         a_list = analyses.split()
         toml_config_initial["general"]["analysis"] = a_list
 
