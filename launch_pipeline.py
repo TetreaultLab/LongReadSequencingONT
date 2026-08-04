e30dc9cdc53ac7ba42a016919ea7b77f2c370098
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2026-08-04 13:00:52 -0400

    time adjust RNA is less than 24h, multiply by two

--- launch_pipeline.py
+++ launch_pipeline.py
@@ -702,6 +702,11 @@ def dorado_basecaller(toml_config, done):
 
         # Scale required job time based on amount of data
         hours = int(size_str) * 0.03
+
+        if toml_config["general"]["seq_type"] == "RNA":
+            if hours <= 24:
+                hours = hours * 2
+
         formatted_time = format_time(hours)
 
         command = [
